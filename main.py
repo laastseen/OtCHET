86770706d6fa1aab476d9d7e622d18a96694f666
laastseen <86373432+laastseen@users.noreply.github.com>
2021-12-04 23:07:55 +0300

    Приложение по практике

---
+++ main.py
@@ -0,0 +1,36 @@
+import sys
+from PyQt5 import QtCore, QtGui, QtWidgets
+from ui import Ui_MainWindow
+
+
+app = QtWidgets.QApplication(sys.argv)
+
+
+MainWindow = QtWidgets.QMainWindow()
+ui = Ui_MainWindow()
+ui.setupUi(MainWindow)
+MainWindow.show()
+
+def chisl():
+    dbl = ui.lineEdit.text()
+    n = int(dbl, 2)
+    print(n)
+
+    Oct = oct(n)
+    Oct = Oct[2:]
+    print(Oct)
+
+    Hex = hex(n)
+    Hex = Hex[2:]
+    Hex = Hex.upper()
+    print(Hex)
+
+    ui.label_6.setText(str(n))
+    ui.label_7.setText(str(Oct))
+    ui.label_8.setText(str(Hex))
+
+    
+
+ui.pushButton.clicked.connect( chisl )
+
+sys.exit(app.exec_())
